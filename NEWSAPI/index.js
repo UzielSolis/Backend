730aa000c099d7832a836e82f39a78cfd1242148
UzielSolis <uziel.solis@iteso.mx>
2024-03-04 12:17:37 -0600

    layouts

--- NEWSAPI/index.js
+++ NEWSAPI/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const routes = require('./src/routes/index.routes');
 const path = require('path');
+const { engine } = require('express-handlebars');
 
 const app = express();
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './src/views');
 
 app.use('/assets', express.static(path.join(__dirname, 'public')));
 app.use(routes);
--- NEWSAPI/src/routes/index.routes.js
+++ NEWSAPI/src/routes/index.routes.js
@@ -1,10 +1,36 @@
 const router = require('express').Router();
 const path = require('path');
+const axios = require('axios');
 
 router.get('', (req, res) => {
     //res.send('api works');
-    const url = path.join(__dirname, '..', 'views', 'index.html');
-    res.sendFile(url);
+    //const url = path.join(__dirname, '..', 'views', 'index.html');
+    //res.sendFile(url);
+    res.render('home', {
+        title: 'Titulo dinamico'
+    });
+});
+
+router.get('/about', (req, res) => {
+    res.render('about', {
+        layout: 'main-login'
+    });
+});
+
+router.get('/tareas', (req, res) => {
+    const userId = req.query.user;
+    if(userId) {
+        const url = `https://jsonplaceholder.typicode.com/todos?userId=${userId}`;
+        axios.get(url).then(response => {
+                res.render('todos', {
+                    userId,
+                    todos: response.data
+                });
+            }).catch();
+
+    } else {
+        res.render('todos');
+    }
 });
 
 module.exports = router;
